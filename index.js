d30dfb3f23362a57edf4899f0bc475cf659bcfdd
Boris Adimov <boris@adimov.ru>
2022-12-12 02:18:10 +0700

    test DO apps

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { v4: uuidV4 } = require('uuid')
 const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
+
 const Airtable = require('airtable');
 require('dotenv').config();
 
@@ -16,7 +18,7 @@ var base = Airtable.base(AIRTABLE_BASE_ID);
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
 
 // When the bot receives a new message, save it to Airtable
-bot.on('message', (ctx) => {
+bot.on(message('text'), (ctx) => {
     base('Maik').create([
         {
           "fields": {
@@ -32,4 +34,36 @@ bot.on('message', (ctx) => {
       });    
 });
 
-bot.launch();
+// bot.launch({
+//     webhook: {
+//       // Public domain for webhook; e.g.: example.com
+//       domain: webhookDomain,
+  
+//       // Port to listen on; e.g.: 8080
+//       port: port,
+  
+//       // Optional path to listen for.
+//       // `bot.secretPathComponent()` will be used by default
+//       hookPath: webhookPath,
+  
+//       // Optional secret to be sent back in a header for security.
+//       // e.g.: `crypto.randomBytes(64).toString("hex")`
+//       secretToken: randomAlphaNumericString,
+//     },
+//   });
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+
+const express = require('express');
+const app = express();
+
+// get our port
+const port = process.env.PORT || 3000;
+
+// applicaton code goes here
+
+// have node listen on our port
+app.listen(port, () => console.log(`App listening on port ${port}!`));
